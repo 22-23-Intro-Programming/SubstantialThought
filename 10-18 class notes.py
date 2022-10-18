b9ed022d44c61e1fb48ada0ca7bfc334fdfec333
Jacob Tuckerman <jacob_tuckerman@m018610.aw.org>
2022-10-18 10:43:44 -0700

    Class notes from today

---
+++ 10-18 class notes.py
@@ -0,0 +1,41 @@
+
+def add3(x):
+    #anything here is a part of add3
+
+    result = x + 3
+    return result
+
+def main():
+    #print("Salutations world!")
+    """print(1234)
+
+    hello = 'Sup class'
+    numStudents = 13
+    rint(hello + " Hey, " + str(numStudents)+ " = amount  of students today.")"""
+
+
+
+    #x = input("Whats ur fav #?")
+    '''x = int(x)
+    if x > 10:
+        print(x / 2)
+    else:
+        print(x + 1)'''
+
+    res = add3(12)
+    print(res)
+    #print(add3(100))
+
+    myString = "Tuckerman"
+    firstChar = myString[0]
+    seventh = myString [7]
+    #print(firstChar)
+    #print(seventh)
+
+    for char in myString:
+        print(char)
+
+
+
+if __name__ == "__main__":
+    main()
