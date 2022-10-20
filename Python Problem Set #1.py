2cef69777d14d8e936029a2fee7704cc58ce782b
Jacob Tuckerman <jacob_tuckerman@m018610.aw.org>
2022-10-20 10:15:55 -0700

    Python Practice #1

--- Python Problem Set #1.py
+++ Python Problem Set #1.py
@@ -1,5 +1,3 @@
-Python 3.11.0a5 (v3.11.0a5:c4e4b91557, Feb  3 2022, 14:54:01) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
 def name():
     nameInput = input('What is your name?')
     if nameInput == ('Mr. Considine'):
