6ac522de65fdac6ec11a881ba341e83932873fc7
Jacob Tuckerman <jacob_tuckerman@m018610.aw.org>
2022-11-01 09:48:12 -0700

    Loops and Stings

---
+++ Hw 3.py
@@ -0,0 +1,25 @@
+def main():
+
+    myList = [7,7,3,2,1]
+
+    '''for i in range(4):
+        if i + (i+1) == 14:
+            print (True)
+            break
+
+    else:
+        print (False)'''
+
+    
+
+
+        
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
